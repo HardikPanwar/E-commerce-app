c08d99fb8c220194a155eeecaaff784a70f1b722
Hardik panwar <hardikpanwar63@gmail.com>
2023-08-02 22:30:47 +0530

    login & signup UI added

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Login from '../features/auth/component/Login'
+function LoginPage() {
+  return (
+    <Login/>
+  )
+}
+
+export default LoginPage
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signup from '../features/auth/component/Signup'
+
+function SignupPage() {
+  return (
+    <Signup/>
+  )
+}
+
+export default SignupPage
