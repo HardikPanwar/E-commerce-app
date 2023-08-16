3acb74884bbb12333f437d5eedff4c9ca632d801
Hardik panwar <hardikpanwar63@gmail.com>
2023-08-16 21:34:21 +0530

    Login layout added

--- src/features/auth/component/Login.js
+++ src/features/auth/component/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const dispatch = useDispatch();
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-20 w-auto bg-transparent"
@@ -77,6 +77,6 @@ export default function Login() {
             </a>
           </p>
         </div>
-      </div>
+    </div>
   );
 }
