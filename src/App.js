df9852140e48981996708f6d90010d12aa03aec3
Hardik panwar <hardikpanwar63@gmail.com>
2023-11-11 17:32:39 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,37 @@ import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "signup",
+    element: <SignupPage/>,
+  },
+  
+  
+  
+]);
 
 function App() {
   return (
     <div className="App">
-      <LoginPage/>
+      <RouterProvider router={router} />
+      {/* <LoginPage/> */}
       {/* <Home/> */}
       {/* <SignupPage/> */}
     </div>
